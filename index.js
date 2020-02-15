166bca975b1a60b274bdf0e72607af5f0f8fdac8
Diogo Torres <diogo.torres93@gmail.com>
2020-02-14 23:07:36 -0300

    test finished

--- index.js
+++ index.js
@@ -4,24 +4,52 @@ const Bluebird  = require('bluebird');
 const Cheerio   = require('cheerio');
 const Request   = Bluebird.promisify(require('request'));
 
-(async () => {
+const BASE_URL = 'https://github.com/facebook';
 
+async function getPage(url) {
   let response = await Request({
-    url: 'https://github.com/facebook',
+    url,
     method: "GET",
     headers: { 'Cache-Control': 'no-cache' },
     timeout: 10000
   })
+
+  return response.body;
+}
+
+(async () => {
+
+  console.log('Scrapping first page');
+  let $ = Cheerio.load(await getPage(BASE_URL));
   
-  console.log(response.statusCode);
+  let name = $('h1').text().trim();
+  let url = $('ul a.text-gray-dark').attr('href').trim();
+  let location = $('.has-location span').text().trim();
 
-  let $ = Cheerio.load(response.body);
-  
-  let h1 = $('h1').text().trim();
+  let result = {
+    name,
+    url,
+    location,
+    repos: []
+  }
 
-  console.log(h1);
+  $('a[data-hovercard-type=repository]').toArray().forEach(el => {
+    result.repos.push({ name: el.children[0].data.trim() });
+  });
 
-  // Your Code Here!
+  let pageCount = Number($('.pagination .current').attr('data-total-pages'));
+  if(pageCount && pageCount > 1) {
+    for(let i = 2; i <= pageCount; i++) {
+      console.log(`Scrapping page ${i}`);
+      $ = Cheerio.load(await getPage(`${BASE_URL}?page=${i}`));
+      $('a[data-hovercard-type=repository]').toArray().forEach(el => {
+        result.repos.push({ name: el.children[0].data.trim() });
+      });
+    }
+  }
+
+  console.log(result);
+  return result;
 
 })()
 
